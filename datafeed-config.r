1cf61035039f5452d3d84f5cc379df30a00fa90b
censix <censix@users.noreply.github.com>
2015-01-09 17:12:10 +0100

    Update datafeed-config.r

--- datafeed-config.r
+++ datafeed-config.r
@@ -8,10 +8,10 @@ IBclientid = 88
 #				exchange, 
 #				primary,
 #				currency (EUR),
-#				outputbarsize=5    # ignored. 
-#				keepcsvhistory= FALSE   # ignored
-#				keepoutputhistory=TRUE  #if TRUE the contents of the *-shared.bin file will first be appended to a history
-#                                   #file (STREAM??.rdata in xts format) before is is overwritten by new data
+#				outputbarsize=5    # IGNORED. 
+#				keepcsvhistory= FALSE   # IGNORED
+#				keepoutputhistory=TRUE  #if TRUE the contents of the *-shared.bin file will first be appended to a 
+#                                                       # history file (STREAM??.rdata in xts format) before is is overwritten by new data
 
 IBcontractSpecs <- list(
 # Some Xetra stocks
@@ -39,8 +39,8 @@ IBstreamsymbolSpecs <- list( 	 stream1=c("BMW","DB1","DBK","DPB","DPW","LHA","RW
 
 
 #  * Directories
-IBdatafeedRawDir =     'datafeed-raw'     # contains raw output data from IB:           'SYMBOL-raw.csv'
-IBdatafeedShareDir =   'datafeed-shared'  # contains file-based shared mmap structures: 'SYMBOL-EXCHANGE-shared.bin'
+IBdatafeedRawDir =     'datafeed-raw'     #  NOT USED
+IBdatafeedShareDir =   'datafeed-shared'  # contains file-based shared mmap structures, i.e.'stream1.bin'
 IBdatafeedHistoryDir = 'rdata/intraday/ibrokers/XXsec'  # contains historical SYMBOL_xts data in files named: 'SYMBOL.rdata'
 ## WARNING ## note that new data will only be appended to the .rdata file at the beginning of the NEXT session. Maybe change this, to happen at the END of THIS session
 # make a symbolic link: ln -s ~/portfolios/rdata ./rdata
